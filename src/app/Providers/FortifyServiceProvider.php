c0305087e53d73ebfb17c441f83e3e499a4d954e
ai2525desu <umi08062525@gmail.com>
2025-04-17 20:01:09 +0900

    FortifyServiceProvider.php修正完了

--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -28,19 +28,26 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // 下記はApp\Actions\Fortifyのファイルをクラスとして読み込んでいる。
+        // 新規ユーザー登録用
         Fortify::createUsersUsing(CreateNewUser::class);
-        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
-        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
-        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
 
-        RateLimiter::for('login', function (Request $request) {
-            $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
+        // 初期値で入っていた内容は使用しない機能のため今回削除。下記は追記。
 
-            return Limit::perMinute(5)->by($throttleKey);
+        // Getメソッドで/registerにアクセスしたときに表示するView
+        Fortify::registerView(function () {
+            return view('auth.register');
         });
 
-        RateLimiter::for('two-factor', function (Request $request) {
-            return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        // Getメソッドで/loginにアクセスしたときに表示するView
+        Fortify::loginView(function () {
+            return view('auth.login');
+        });
+
+        // login処理の実行回数を1分あたり１０回までに制限
+        RateLimiter::for('login', function (Request $request) {
+            $email = (string) $request->email;
+            return Limit::perMinute(10)->by($email . $request->ip());
         });
     }
 }
